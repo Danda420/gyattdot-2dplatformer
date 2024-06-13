a4a1a6df1de51af7995d962e2c1e54c75f189aef
Danda <dandagaharu77@gmail.com>
2024-06-13 21:54:30 +0700

    fix void & fire errors

--- scenes/fire.gd
+++ scenes/fire.gd
@@ -4,7 +4,8 @@ extends Area2D
 
 func _ready():
 	animated_sprite_2d.play("default")
-	connect("body_entered", Callable(self, "_on_body_entered"))
+	if not is_connected("body_entered", Callable(self, "_on_body_entered")):
+		connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
--- scenes/void.gd
+++ scenes/void.gd
@@ -3,7 +3,8 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	connect("body_entered", Callable(self, "_on_body_entered"))
+	if not is_connected("body_entered", Callable(self, "_on_body_entered")):
+		connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body):
 	if body is CharacterBody2D:
