582fdb7bbc31ae53d0f2b6933968083b91e482d2
Danda <dandagaharu77@gmail.com>
2024-06-13 17:27:19 +0700

    redesign main char & add dash

--- scenes/main_char_lvl2.gd
+++ scenes/main_char_lvl2.gd
@@ -1,43 +1,65 @@
 extends CharacterBody2D
 
-
+# Constants
 const SPEED = 300.0
 const JUMP_VELOCITY = -550.0
+const DASH_SPEED = 800.0
+const DASH_DURATION = 0.2
 
+# Variables
 @onready var sprite_2d = $Sprite2D
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var is_dashing = false
+var dash_timer = 0.0
 
 func _physics_process(delta):
-	
-	if (velocity.x > 1 || velocity.x < -1):
-		sprite_2d.animation = "running"
-	else:
-		sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
+	else:
+		if not is_dashing:
+			if velocity.x > 1 or velocity.x < -1:
+				sprite_2d.animation = "running"
+			else:
+				sprite_2d.animation = "default"
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing = false
 	else:
-		velocity.x = move_toward(velocity.x, 0, 10)
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, 10)
+
+		# Check for dash input
+		if Input.is_action_just_pressed("dash") and not is_dashing:
+			start_dash(direction)
+
+	if is_dashing:
+		velocity.x = direction * DASH_SPEED
 
 	move_and_slide()
-	
+
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
 
+func start_dash(direction: float):
+	if direction == 0:
+		return  # Prevent dashing with no input direction
+	if is_on_floor():
+		sprite_2d.animation = "dash"
+		is_dashing = true
+		dash_timer = DASH_DURATION
+		velocity.x = direction * DASH_SPEED
 
 func respawn():
 	global_position = Vector2(100, 100)
